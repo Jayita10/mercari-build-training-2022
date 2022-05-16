4bdcb37de071540837257fe2d5b29ca145c5ce7b
Jayita10 <jayitapramanik2018@gmail.com>
2022-05-16 19:38:02 +0530

    completed task 3.3

--- python/main.py
+++ python/main.py
@@ -6,6 +6,9 @@ from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+
+FILENAME = "items.json"
+
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
@@ -19,22 +22,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def write_json(new_data, filename="items.json"):
-    with open(filename, 'r') as file:
-        file_data = json.load(file)
-    with open(filename, 'w') as file:
-        file_data["items"].append(new_data)
-        json.dump(file_data, file)
 
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
+@app.get("/items")
+def get_items():
+    return load_file_to_json()
+
 @app.post("/items")
 def add_item(name: str = Form(...), category: str = Form(...)):
     write_json({"name": name, "category": category})
-    logger.info(f"Receive item: {name} in {category}")
-    return {"message": f"item received: {name} in {category}"}
+    logger.info(f"Receive item: {name}")
+    return {"message": f"item received: {name}"}
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
@@ -49,3 +50,15 @@ async def get_image(image_filename):
         image = images / "default.jpg"
 
     return FileResponse(image)
+
+
+def load_file_to_json():
+    with open(FILENAME, 'r') as file:
+        file_data = json.load(file)
+        return file_data
+
+def write_json(new_data):
+        file_data = load_file_to_json()
+        with open(FILENAME, 'w') as file:
+            file_data["items"].append(new_data)
+            json.dump(file_data, file)
